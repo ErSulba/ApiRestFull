a708271648727e8c9ed4dbd8a4fb967a2883e3fd
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2018-05-03 00:09:09 -0400

    Feature: added a route that returns a transaction list for a given Seller

---
+++ app/Http/Controllers/Seller/SellerTransactionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Seller;
+
+
+class SellerTransactionController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Seller $seller
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Seller $seller)
+    {
+        $transaction = $seller->products()
+            ->whereHas('transactions')
+            ->with('transactions')
+            ->get()
+            ->pluck('transactions')
+            ->collapse();
+
+        return $this->showAll($transaction);
+    }
+
+
+}
